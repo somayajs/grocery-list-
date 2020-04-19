370dd0fc4175f4c342d7651dd3e15956e60a73e7
somaya224 <somaya224@yahoo.com>
2020-04-19 08:51:35 -0400

    add ngFor and mgIf directives togroceries-list.component.html

--- src/app/groceries/groceries-list.component.ts
+++ src/app/groceries/groceries-list.component.ts
@@ -5,4 +5,35 @@ import { Component } from '@angular/core';
   })
 export class GroceriesListComponent {
   pageTitle: string = 'Grocery List';
+  imageWidth : number = 50;
+  imageMargin: number = 2;
+  groceries: any[] = [
+    {
+      "groceryID": 1,
+      "groceryName": "Brocolli",
+      "groceryCode": "BRC-024",
+      "quantity": 2,
+      "price": 5.99,
+      "gotIt": 1,
+      "imageUrl": "assets/images/broccoli.png"
+    },
+    {
+      "groceryID": 2,
+      "groceryName": "Slice of Cake",
+      "groceryCode": "SLC-001",
+      "quantity": 6,
+      "price": 1.99,
+      "gotIt": 0,
+      "imageUrl": "assets/images/cake-slice.png"
+    },
+    {
+      "groceryID": 3,
+      "groceryName": "Watermelon",
+      "groceryCode": "WTM-102",
+      "quantity": 1,
+      "price": 8.99,
+      "gotIt": 0,
+      "imageUrl": "assets/images/watermelon.png"
+    }
+  ]
 }
