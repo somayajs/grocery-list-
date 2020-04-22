abec0180e097cdbb7f93d72fadc08dd3306f2a46
somaya224 <somaya224@yahoo.com>
2020-04-22 09:39:17 -0400

    install fortawesome and use the checked and unckeched circle in the groceries component also made a nested sharecomponent checked and unchecked circle component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
 import { GroceriesListComponent  } from './groceries/groceries-list.component';
+import { CheckCircleComponent} from './shared/check-circle.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 @NgModule({
   declarations: [
     AppComponent,
-    GroceriesListComponent
+    GroceriesListComponent,
+    CheckCircleComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/check-circle.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { faCircle, faCheckCircle } from '@fortawesome/free-regular-svg-icons';
+@Component({
+  selector: 'app-check-circle',
+  templateUrl: 'check-circle.component.html',
+  styleUrls: ['check-circle.component.css']
+})
+export class CheckCircleComponent {
+  faCheckCircle = faCheckCircle;
+  faCircle = faCircle;
+  @Input() isChecked: boolean;
+  onClick() {
+
+    this.isChecked = !this.isChecked;
+  }
+}
